6effb7fd73f105cd17d81f2ef8804e61f7edf38a
AranBeitia <arantza.beitia@gmail.com>
2021-10-03 19:26:52 +0200

    message

--- src/components/TodoForm/schema.js
+++ src/components/TodoForm/schema.js
@@ -1,7 +1,7 @@
 import * as yup from 'yup'
 
 export const TodoItemSchema = yup.object().shape({
-	title: yup.string().required(),
+	title: yup.string().required('Title is a required field'),
 	description: yup.string().min(5).ensure(),
 	priority: yup.number().min(0).max(2).default(0),
 })
